7941669246af104d71cfc5fc6f0bedb11837cc60
sathisk15 <rsathisk15@gmail.com>
2026-02-24 07:46:55 +0100

    NavBar user details updated

--- frontend/src/shared/componenets/NavBar.jsx
+++ frontend/src/shared/componenets/NavBar.jsx
@@ -4,23 +4,24 @@ import { useAuthStore } from '../../store/useAuthStore';
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const { logout, user } = useAuthStore((state) => {
-    console.log(state);
-    return state;
-  });
+  const logout = useAuthStore((state) => state.logout);
+  const user = useAuthStore((state) => state.user);
 
   const handleLogout = () => {
     logout();
     navigate('/login');
   };
+  console.log(user);
   return (
     <header className="w-full bg-card border-b border-border px-6 py-4 flex items-center justify-between">
       <span className="text-xl font-bold text-text">Banking App</span>
 
       <div className="flex items-center gap-4">
         <div className="text-right hidden md:block">
-          <p className="text-sm font-medium text-text">Sathiskumar</p>
-          <p className="text-xs text-text/60">test12@gmail.com</p>
+          <p className="text-sm font-medium text-text">
+            {user?.firstname + ' ' + user?.lastname}
+          </p>
+          <p className="text-xs text-text/60">{user.email}</p>
         </div>
 
         <img
