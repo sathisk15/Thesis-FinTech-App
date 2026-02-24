01ffb6fc8e042e685bfc7d7ef0e6ed57e1722936
sathisk15 <rsathisk15@gmail.com>
2026-02-24 07:13:24 +0100

    Logout added to navbar

--- frontend/src/shared/componenets/NavBar.jsx
+++ frontend/src/shared/componenets/NavBar.jsx
@@ -1,7 +1,18 @@
-// src/components/Navbar.tsx
 import { RiLogoutBoxLine } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
+import { useAuthStore } from '../../store/useAuthStore';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { logout, user } = useAuthStore((state) => {
+    console.log(state);
+    return state;
+  });
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
   return (
     <header className="w-full bg-card border-b border-border px-6 py-4 flex items-center justify-between">
       <span className="text-xl font-bold text-text">Banking App</span>
@@ -18,7 +29,10 @@ const Navbar = () => {
           className="w-10 h-10 rounded-full cursor-pointer"
         />
 
-        <RiLogoutBoxLine className="text-2xl cursor-pointer text-text/70" />
+        <RiLogoutBoxLine
+          className="text-2xl cursor-pointer text-text/70"
+          onClick={handleLogout}
+        />
       </div>
     </header>
   );
